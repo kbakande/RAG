41717b9119f093b4be724629e50d4cb4585d790a
Kabeer Akande <46060542+kbakande@users.noreply.github.com>
2025-03-31 07:27:51 +0100

    adds updated app

--- app.py
+++ app.py
@@ -4,25 +4,21 @@ from rag_demo.retriever import Retriever
 from rag_demo.generator import Generator
 from rag_demo.interface import build_ui
 
-# Load and embed
-docs, _ = load_documents("docs")
+# Load and chunk documents
+docs = load_documents("docs", chunk_size=80, overlap=20)
 embedder = Embedder()
 embeddings = embedder.encode(docs)
 
-# Setup retriever
 retriever = Retriever(embeddings)
 retriever.add_docs(docs)
 
-# Generator
 generator = Generator()
 
-# RAG pipeline
 def rag_pipeline(question):
     query_embedding = embedder.encode_query(question)
     context_chunks = retriever.retrieve(query_embedding)
     context = " ".join(context_chunks)
     return generator.generate(context, question)
 
-# Gradio interface
 ui = build_ui(rag_pipeline)
 ui.launch()
