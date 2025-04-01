8ae6ff3fb45d2e76f170ba5792fe0a5ecc0ca1b0
Kabeer Akande <46060542+kbakande@users.noreply.github.com>
2025-04-01 17:20:26 +0100

    modify prompt

--- rag_demo/generator.py
+++ rag_demo/generator.py
@@ -5,15 +5,15 @@ class Generator:
         self.generator = pipeline("text2text-generation", model="google/flan-t5-large")
 
     def generate(self, context, question):
-        prompt = f"""You are an expert AI assistant answering questions strictly based on the provided context. 
-If the answer is not in the context, respond with "I don't know based on the given information."
+        prompt = f"""You are an expert AI assistant answering questions based on the provided context.
 
-Context:
-{context}
+        Context:
+        {context}
 
-Question:
-{question}
+        Question:
+        {question}
 
-Answer:"""
+        Answer:"""
+
         result = self.generator(prompt, max_new_tokens=150)
         return result[0]['generated_text']
